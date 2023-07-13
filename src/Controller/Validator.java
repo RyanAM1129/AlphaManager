1a79586e282c5a6db2f04535b986509d4f95175d
RyanAM1129 <RyanAMacLeod@hotmail.com>
2023-07-12 19:38:43 -0700

    Fixed RegEx support for getName().

--- src/Controller/Validator.java
+++ src/Controller/Validator.java
@@ -7,7 +7,6 @@ public class Validator {
     final static private Scanner input = new Scanner(System.in);
 
     public static int getInt(final int min, final int max) {
-        boolean moveOn = false;
         int check;
 
         System.out.println("Please enter a number between " + min + " and " + max + ":");
@@ -26,16 +25,16 @@ public class Validator {
     }
 
     public static String getName() {
-        Pattern namePattern = Pattern.compile("[A-z]+( ([A-z]\\.|[A-z]+))? [A-z]+-?[A-z]*");
-        input.next();   //Cleans the Scanner for the next input
+        Pattern namePattern = Pattern.compile("^[A-z]+( ([A-z]\\.|[A-z]+))? [A-z]+-?[A-z]*$");
+        input.nextLine();   //Cleans the Scanner for the next input
         String check = input.nextLine();
         Matcher matcher = namePattern.matcher(check);
 
-        if(!matcher.find()) {
-            System.out.println("Please enter a properly formatted name...\n>");
-            return getName();
+        if(matcher.find()) {
+            return check;
         }
-        return check;
+        System.out.println("Please enter a properly formatted name...\n>");
+        return getName();
     }
 
     public static void closeScanner() {
